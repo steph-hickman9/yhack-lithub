7287117d81d11e54c6504913a42fae0a81d0317c
Davey Proctor <david.proctor@yale.edu>
2016-11-13 10:32:28 -0500

    giving up all hope

--- scripts.js
+++ scripts.js
@@ -1,3 +1,87 @@
+// var data = require(['events.json']);
+// function load() {
+// 	var mydata = JSON.parse(data);
+// 	alert(mydata);
+// 	// alert(mydata[0].age);
+// }
+
+// load();
+
+// alert('failed');
+
+
+
+events = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "title": "Toad's Place",
+        "time": "11:00 PM -1:00 AM",
+        "address": "300 York St, New Haven, CT 06511",
+        "party": true
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [41.3115,-72.9296]}
+      },
+    {
+      "type": "Feature",
+      "properties": {
+        "title": "Sigma Chi",
+        "time": "11:00 PM -12:00 AM",
+        "address": "33 Lynwood Pl, New Haven, CT 06511",
+        "party": true
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [41.3110,-72.9329]}
+      },
+    {
+      "type": "Feature",
+      "properties": {
+        "title": "Woolsey Hall",
+        "time": "8:00 PM -9:00 PM",
+        "address": "500 College St, New Haven, CT 06511",
+        "party": false
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [41.30666544,-72.922162978]}
+      },
+    {
+      "type": "Feature",
+      "properties": {
+        "title": "Alpha Epsilon Pi",
+        "time": "8:00 PM -9:00 PM",
+        "address": "395 Crown St., New Haven, CT 06511",
+        "party": false
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [41.30845,-72.93503]}
+      }  
+    ]
+}
+
+console.log(events);
+
+
+
+
+// $.getJSON( "events.json", function( data ) {  
+//     console.log( "JSON Data: " + data);
+//     $.each( data, function( key, val ) {
+//         console.log(key + "value:: " + val );
+//     });
+// });
+
+// var json = require(['events.json'], 'async': false);
+// console.log(['json', json]);
+// alert(json);
+// alert('hello')
+
 
 function initMap() {
 var NHCT = {lat: 41.3083, lng: -72.9279};
